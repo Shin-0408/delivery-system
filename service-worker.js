2311e089d795f9f5138fc562439d163ca1737fff
Shin-0408 <k.k.fukudakoken@icloud.com>
2026-06-29 17:53:49 +0900

    V206  fixes

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,2 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('dsv202').then(c=>c.addAll(['./','index.html','m001.html','m002.html','m003.html','m004.html','complete-card.jpg']))));
+self.addEventListener('install',e=>e.waitUntil(caches.open('dsv205').then(c=>c.addAll(['./','index.html','m001.html','m002.html','m003.html','m004.html','complete-card.jpg']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
